2ba2d1c42fe1723ac12667757ef870302b1c9f73
ZaytsevAndrei <Katote88@gmail.com>
2026-09-22 18:00:29 +0300

    Update social.ts

--- frontend/src/constants/social.ts
+++ frontend/src/constants/social.ts
@@ -1,5 +1,5 @@
 export const DISCORD_URL = 'https://discord.gg/NSPuBH4mZJ';
 
-export const TELEGRAM_URL = 'https://t.me/DragonLostRust';
+export const TELEGRAM_URL = 'https://t.me/dragonLostRust';
 
 export const SUPPORT_EMAIL = 'katote88@outlook.com';
